9ea11553ceb7ae1232706dffbf0ca23b9a71fd92
charan1712 <143885805+charan1712@users.noreply.github.com>
2024-06-02 19:33:26 +0530

    Update jarvis.js

--- jarvis.js
+++ jarvis.js
@@ -2,11 +2,11 @@ const btn = document.querySelector('#listen');
 const content = document.querySelector('#load');
 
 function speak(text){
-   const text_speak =  new SpeechSynthesisUtterance(text);
-   text_speak.rate = 1;
-   text_speak.volume = 1;
-   text_speak.pitch = 1;
-   window.speechSynthesis.speak(text_speak);
+   const textspeak =  new SpeechSynthesisUtterance(text);
+   textspeak.rate = 1;
+   textspeak.volume = 1;
+   textspeak.pitch = 1;
+   window.speechSynthesis.speak(textspeak);
 }
 
 function wishMe(){
@@ -28,6 +28,7 @@ window.addEventListener('load',()=>{
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 let recognition = new SpeechRecognition();
+// recognition.interimResults = true;
  recognition.onresult = (event)=>{
     const currentIndex = event.resultIndex;
     const transcript = event.results[currentIndex][0].transcript;
@@ -41,7 +42,7 @@ let recognition = new SpeechRecognition();
  });
 
  function takeCommand(message){
-    if(message.includes('hey') || message.includes('hello')){
+     if(message.includes('hey') || message.includes('hello')){
         speak('Hello Sir, How May I help You')
     }
     else if(message.includes('what is my name'))
@@ -52,10 +53,43 @@ let recognition = new SpeechRecognition();
         {
             speak('My name is jarvis, i am, a, Ai')
         }
-    else if(message.includes(`${message}`) || message.includes(`${message}`)){
+    else if(message.includes('calculator')){
+            window.open('Calculator')
+            speak('opening Calculator')
+        }
+    
+    else if(message.includes("what is") || message.includes("who is") || message.includes("where is") || message.includes("about")){
+        window.open(`https://www.google.com/search?q=${message.replace(" ","+")}`),"_blank"
+        speak("This is What i found on Internet")
+    }
+    else if(message.includes(`${message}`)){
         message = message.split(" ");
         console.log(message.length);
-        window.open(`http://${message[message.length-1]}.com`,"_blank");
+        window.open(`com.google.android.${message[message.length-1]}`),"_blank";
+        speak(`opening${message[message.length-1]}`)
+    }
+    else if(message.includes(`${message}`)){
+        message = message.split(" ");
+        console.log(message.length);
+        window.open(`com.${message[message.length-1]}`),"_blank";
+        speak(`opening${message[message.length-1]}`)
+    }
+    else if(message.includes(`${message}`)){
+        message = message.split(" ");
+        console.log(message.length);
+        window.open(`com.${message[message.length-1]}.android.`),"_blank";
+        speak(`opening${message[message.length-1]}`)
+    }
+    else if(message.includes(`${message}`)){
+        message = message.split(" ");
+        console.log(message.length);
+        window.open(`com.pubg,imobile`),"_blank";
+        speak(`opening${message[message.length-1]}`)
+    }
+    else if(message.includes(`${message}`)){
+        message = message.split(" ");
+        console.log(message.length);
+        window.open(`http://${message[message.length-1]}.com`),"_blank";
         speak(`opening${message[message.length-1]}`)
     }
  }
